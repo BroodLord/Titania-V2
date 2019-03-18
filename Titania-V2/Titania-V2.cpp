e430dba3cb14c6730f49563aee5ba279383a3e4f
kyle <KClifford1@uclan.ac.uk>
2019-03-18 14:45:16 +0000

    working

--- Titania-V2/Titania-V2.cpp
+++ Titania-V2/Titania-V2.cpp
@@ -54,8 +54,8 @@ void main()
 	myEngine->StartFullscreen(1920, 1080);
 
 	// Add default folder for meshes and other media
-	myEngine->AddMediaFolder("C:\\ProgramData\\TL-Engine\\Media");
-	myEngine->AddMediaFolder("C:\\Users\\danny\\Desktop\\Titania-V2\\Titania-V2\\Media");
+	myEngine->AddMediaFolder(".\\Media");
+	//myEngine->AddMediaFolder("C:\\Users\\danny\\Desktop\\Titania-V2\\Titania-V2\\Media");
 
 	/**** Set up your scene here ****/
 	ICamera* playerCamera = myEngine->CreateCamera(kManual);
